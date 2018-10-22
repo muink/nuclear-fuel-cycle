06e1b39690a3de92d983db2e5678207d2885cc37
muink <hukk1996@gmail.com>
2018-10-22 10:38:47 +0800

    严格模式 处理大部分反应堆事件

---
+++ control.lua
@@ -0,0 +1,169 @@
+--[[Init Block]]--
+--------------------------------
+
+--startup
+local EXACTING_MODE = settings.startup["nuclear-fuel-cycle_exacting-mode"].value
+
+--runing
+local MINING_LIMIT
+local POLLUTION_MULTIPLIER
+local DESTRUCTION_POLLUTION
+local CRITICAL_TEMPERATURE
+--------
+local DEBUG_INFO = false
+local FULL_UPDATE_INTERVAL = 40 --full update cycle, lowering this value increases performance overhead
+local PER_TICK_REACTOR_CONSUMPTION = 40*1e+6/60
+
+--events
+local e=defines.events
+local built_events = {e.on_built_entity, e.on_robot_built_entity}
+local mining_events = {e.on_pre_player_mined_item, e.on_robot_pre_mined} --after finishes mining an entity before results are collected
+local mined_events = {e.on_player_mined_entity, e.on_robot_mined_entity} --after results are collected before entity is destroyed
+local mineditem_events = {e.on_player_mined_item, e.on_robot_mined} --at result items given to the player/robot
+local died_events = {e.on_entity_died}
+
+
+
+--[[Sub function Block]]--
+--------------------------------
+
+local math_floor = math.floor
+local math_ceil = math.ceil
+local math_modf = math.modf
+
+local function load_settings()
+	MINING_LIMIT = settings.global["nuclear-fuel-cycle_mining-limit"].value
+	POLLUTION_MULTIPLIER = settings.global["nuclear-fuel-cycle_pollution-multiplier"].value
+	DESTRUCTION_POLLUTION = settings.global["nuclear-fuel-cycle_destruction-pollution"].value
+	CRITICAL_TEMPERATURE = settings.global["nuclear-fuel-cycle_critical-temperature"].value
+	DEBUG_INFO = settings.global["nuclear-fuel-cycle_debug_mode"].value
+end
+
+local function debug_log(...)
+	if DEBUG_INFO then game.print(...) end
+end
+
+--reactor running? and remaining burning fuel value?
+local function reactor_runing_status(reactor)
+	--return NOT running or BURNED fuel value
+	return reactor.burner.remaining_burning_fuel > 0 and reactor.burner.currently_burning.fuel_value - reactor.burner.remaining_burning_fuel, reactor.burner.remaining_burning_fuel > 0 and reactor.burner.currently_burning.fuel_value
+end
+
+
+
+--[[Main Events function Block]]--
+--------------------------------
+
+--called when an reactor entity is destruction
+local function died(event)
+	local entity = event.entity
+	if entity.type == "reactor" then
+		debug_log("Find a reactor has been died.")
+		local id = entity.unit_number
+		if reactor_runing_status(entity) then
+			debug_log("Find an active reactor has been died.")
+			--[[
+			game.print(entity.surface.create_entity{
+				name = "logistic-chest-requester",
+				position = entity.position,
+				force = entity.force,
+				request_filters = {
+					{index = 1, name = "iron-plate", count = reactor_runing_status(entity)}
+				}
+			}.unit_number)
+			--]]
+
+			--create destruction pollution
+			if DESTRUCTION_POLLUTION then
+				debug_log("Destroyed pollution has released.")
+				--the value of fuel has been burned
+				local burned_fuel=math_modf(reactor_runing_status(entity)/1e+6)
+				entity.surface.pollute(entity.position, burned_fuel * POLLUTION_MULTIPLIER)
+				debug_log("destroyed pollution: " .. burned_fuel * POLLUTION_MULTIPLIER, {r=1,g=0.25,b=0,a=1})
+			end
+		end
+		global.reactors[id] = nil
+	end
+end
+
+--[[
+--called when mines entity
+local function mineditem(event)
+	local item = event.item_stack
+	debug_log{"", game.item_prototypes[item.name].localised_name, " given to the player/robot."}
+end
+--]]
+
+--called when an reactor entity is mined
+local function mined(event)
+	local entity = event.entity
+	if entity.type == "reactor" then
+		debug_log("Remove reactor from table")
+		local id = entity.unit_number
+		--if entity.die() then debug_log "kill the entity before destroyed." end --result items will be collect, corpse will generate
+		global.reactors[id] = nil
+	end
+end
+
+--[[
+--called when mining an reactor entity
+local function mining(event)
+	local entity = event.entity
+	if entity.type == "reactor" then
+		local id = entity.unit_number
+		if MINING_LIMIT and reactor_runing_status(entity) then
+			--to limit mining
+			entity.minable = false
+			return
+		end
+		--if entity.die() then debug_log "kill the entity before results are collected." end --result items will not be collect, corpse will generate
+		global.reactors[id] = nil
+	end
+end
+--]]
+
+--called when an reactor entity is build
+local function built(event)
+	local entity = event.created_entity
+	if entity.type == "reactor" then
+		debug_log("Add reactor to table")
+		local id = entity.unit_number
+		global.reactors[id] = {reactor = entity, last_burned = false}
+	end
+end
+
+local function setup_global()
+	game.print({"message.exacting-mode-enabled"}, {r=1,g=1,b=0,a=1})
+	global = {reactors = {}}
+
+	--traverse all surface on map to find all nuclear reactors
+	for _, surface in pairs(game.surfaces) do
+		for _, reactor in pairs(surface.find_entities_filtered{type="reactor"}) do
+			built({created_entity = reactor})
+		end
+	end
+end
+
+
+
+--[[Main Block]]--
+--------------------------------
+
+load_settings()
+
+if EXACTING_MODE then
+
+--setup event handler
+script.on_init(setup_global)
+script.on_configuration_changed(setup_global)
+script.on_event(e.on_runtime_mod_setting_changed, load_settings)
+
+
+--all game build/remove events handler
+script.on_event(built_events, built)
+--script.on_event(mining_events, mining)
+script.on_event(mined_events, mined)
+--script.on_event(mineditem_events, mineditem)
+script.on_event(died_events, died)
+
+end
--- settings.lua
+++ settings.lua
@@ -1,5 +1,6 @@
 local prefix = "nuclear-fuel-cycle_"
 
+--startup
 data:extend({
 	{
 		type = "string-setting",
@@ -8,5 +9,59 @@ data:extend({
 		default_value = 20, --30/15*10 based on critical mass
 		allowed_values = {"13", "15", "17", "20", "23", "27", "30"},
 		order = "a"
+	},
+	{
+		type = "bool-setting",
+		name = prefix .. "exacting-mode",
+		setting_type = "startup",
+		default_value = true,
+		order = "b"
+	}
+})
+
+
+--runtime-global
+data:extend({
+---------runing
+	{
+		type = "bool-setting",
+		name = prefix .. "mining-limit",
+		setting_type = "runtime-global",
+		default_value = true,
+		order = "a"
+	},
+	{
+		type = "double-setting",
+		name = prefix .. "pollution-multiplier",
+		setting_type = "runtime-global",
+		default_value = 30,
+		minimum_value = 0.0,
+		maximum_value = 1.0e+8,
+		order = "a-b"
+	},
+	{
+		type = "bool-setting",
+		name = prefix .. "destruction-pollution",
+		setting_type = "runtime-global",
+		default_value = true,
+		order = "a-c"
+	},
+---------critical temperature
+	{
+		type = "double-setting",
+		name = prefix .. "critical-temperature",
+		setting_type = "runtime-global",
+		default_value = 998.0,
+		minimum_value = 900,
+		maximum_value = 1000,
+		order = "b"
+	},
+---------misc
+	{
+		type = "bool-setting",
+		name = prefix .. "debug_mode",
+		setting_type = "runtime-global",
+		default_value = false,
+		order = "c"
 	}
 })
