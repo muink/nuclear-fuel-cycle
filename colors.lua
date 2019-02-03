ab7e3686c6e7913d39eb2c3c821265cd0322cf73
muink <hukk1996@gmail.com>
2019-02-03 15:52:26 +0800

    简化颜色表

--- colors.lua
+++ colors.lua
@@ -12,8 +12,21 @@ data:extend({
 })
 
 
+for key in pairs(colors) do
+	colors[key] = {
+		subgroup = "reactor-mask",
+		order = "b",
+		item_icon = {icon = "__nuclear-fuel-cycle__/graphics/icons/nuclear-reactor-" .. key .. ".png", size = 32},
+		entity_icon = {icon = "__nuclear-fuel-cycle__/graphics/icons/nuclear-reactor-" .. key .. ".png", size = 32},
+		entity_light_img = {img = "__nuclear-fuel-cycle__/graphics/entity/reactor-lights-color-" .. key .. ".png", width = 160, height = 160},
+		entity_light_hrimg = {img = "__nuclear-fuel-cycle__/graphics/entity/hr-reactor-lights-color-" .. key .. ".png", width = 320, height = 320, scale = 0.5},
+		entity_glow = {intensity = 0.6, size = 11, shift = {0.0, 0.0}, color = {r = 1.0, g = 1.0, b = 1.0}}
+	}
+end
+
+
 --normal
-colors.normal = {
+colors["normal"] = {
 	subgroup = "reactor-mask",
 	order = "a[normal]",
 	item_icon = {icon = data.raw.item["nuclear-reactor"].icon, size = 32},
@@ -24,100 +37,41 @@ colors.normal = {
 }
 
 --red
-colors.red = {
-	subgroup = "reactor-mask",
-	order = "b-a-a[red]",
-	item_icon = {icon = "__nuclear-fuel-cycle__/graphics/icons/nuclear-reactor-red.png", size = 32},
-	entity_icon = {icon = "__nuclear-fuel-cycle__/graphics/icons/nuclear-reactor-red.png", size = 32},
-	entity_light_img = {img = "__nuclear-fuel-cycle__/graphics/entity/reactor-lights-color-red.png", width = 160, height = 160},
-	entity_light_hrimg = {img = "__nuclear-fuel-cycle__/graphics/entity/hr-reactor-lights-color-red.png", width = 320, height = 320, scale = 0.5},
-	entity_glow = {intensity = 0.6, size = 11, shift = {0.0, 0.0}, color = {r = 1.0, g = 0.0, b = 0.0}}
-}
+colors["red"].order = "b-a-a[red]"
+colors["red"].entity_glow = {intensity = 0.6, size = 11, shift = {0.0, 0.0}, color = {r = 1.0, g = 0.0, b = 0.0}}
 
 --orangered
-colors.orangered = {
-	subgroup = "reactor-mask",
-	order = "b-a-b[orangered]",
-	item_icon = {icon = "__nuclear-fuel-cycle__/graphics/icons/nuclear-reactor-orangered.png", size = 32},
-	entity_icon = {icon = "__nuclear-fuel-cycle__/graphics/icons/nuclear-reactor-orangered.png", size = 32},
-	entity_light_img = {img = "__nuclear-fuel-cycle__/graphics/entity/reactor-lights-color-orangered.png", width = 160, height = 160},
-	entity_light_hrimg = {img = "__nuclear-fuel-cycle__/graphics/entity/hr-reactor-lights-color-orangered.png", width = 320, height = 320, scale = 0.5},
-	entity_glow = {intensity = 0.7, size = 11, shift = {0.0, 0.0}, color = {r = 0.88, g = 0.24, b = 0.0}}
-}
+colors["orangered"].order = "b-a-b[orangered]"
+colors["orangered"].entity_glow = {intensity = 0.7, size = 11, shift = {0.0, 0.0}, color = {r = 0.88, g = 0.24, b = 0.0}}
 
 --orange
-colors.orange = {
-	subgroup = "reactor-mask",
-	order = "b-b-a[orange]",
-	item_icon = {icon = "__nuclear-fuel-cycle__/graphics/icons/nuclear-reactor-orange.png", size = 32},
-	entity_icon = {icon = "__nuclear-fuel-cycle__/graphics/icons/nuclear-reactor-orange.png", size = 32},
-	entity_light_img = {img = "__nuclear-fuel-cycle__/graphics/entity/reactor-lights-color-orange.png", width = 160, height = 160},
-	entity_light_hrimg = {img = "__nuclear-fuel-cycle__/graphics/entity/hr-reactor-lights-color-orange.png", width = 320, height = 320, scale = 0.5},
-	entity_glow = {intensity = 0.8, size = 11, shift = {0.0, 0.0}, color = {r = 1.0, g = 0.5, b = 0.0}}
-}
+colors["orange"].order = "b-b-a[orange]"
+colors["orange"].entity_glow = {intensity = 0.8, size = 11, shift = {0.0, 0.0}, color = {r = 1.0, g = 0.5, b = 0.0}}
 
 --yelloworange
-colors.yelloworange = {
-	subgroup = "reactor-mask",
-	order = "b-b-b[yelloworange]",
-	item_icon = {icon = "__nuclear-fuel-cycle__/graphics/icons/nuclear-reactor-yelloworange.png", size = 32},
-	entity_icon = {icon = "__nuclear-fuel-cycle__/graphics/icons/nuclear-reactor-yelloworange.png", size = 32},
-	entity_light_img = {img = "__nuclear-fuel-cycle__/graphics/entity/reactor-lights-color-yelloworange.png", width = 160, height = 160},
-	entity_light_hrimg = {img = "__nuclear-fuel-cycle__/graphics/entity/hr-reactor-lights-color-yelloworange.png", width = 320, height = 320, scale = 0.5},
-	entity_glow = {intensity = 0.6, size = 11, shift = {0.0, 0.0}, color = {r = 1.0, g = 0.65, b = 0.0}}
-}
+colors["yelloworange"].order = "b-b-b[yelloworange]"
+colors["yelloworange"].entity_glow = {intensity = 0.6, size = 11, shift = {0.0, 0.0}, color = {r = 1.0, g = 0.65, b = 0.0}}
 
 --yellow
-colors.yellow = {
-	subgroup = "reactor-mask",
-	order = "b-c-a[yellow]",
-	item_icon = {icon = "__nuclear-fuel-cycle__/graphics/icons/nuclear-reactor-yellow.png", size = 32},
-	entity_icon = {icon = "__nuclear-fuel-cycle__/graphics/icons/nuclear-reactor-yellow.png", size = 32},
-	entity_light_img = {img = "__nuclear-fuel-cycle__/graphics/entity/reactor-lights-color-yellow.png", width = 160, height = 160},
-	entity_light_hrimg = {img = "__nuclear-fuel-cycle__/graphics/entity/hr-reactor-lights-color-yellow.png", width = 320, height = 320, scale = 0.5},
-	entity_glow = {intensity = 0.6, size = 9.9, shift = {0.0, 0.0}, color = {r = 1.0, g = 0.8, b = 0.0}}
-}
+colors["yellow"].order = "b-c-a[yellow]"
+colors["yellow"].entity_glow = {intensity = 0.6, size = 9.9, shift = {0.0, 0.0}, color = {r = 1.0, g = 0.8, b = 0.0}}
 
 --yellowgreen
-colors.yellowgreen = {
-	subgroup = "reactor-mask",
-	order = "b-c-b[yellowgreen]",
-	item_icon = {icon = "__nuclear-fuel-cycle__/graphics/icons/nuclear-reactor-yellowgreen.png", size = 32},
-	entity_icon = {icon = "__nuclear-fuel-cycle__/graphics/icons/nuclear-reactor-yellowgreen.png", size = 32},
-	entity_light_img = {img = "__nuclear-fuel-cycle__/graphics/entity/reactor-lights-color-yellowgreen.png", width = 160, height = 160},
-	entity_light_hrimg = {img = "__nuclear-fuel-cycle__/graphics/entity/hr-reactor-lights-color-yellowgreen.png", width = 320, height = 320, scale = 0.5},
-	entity_glow = {intensity = 0.6, size = 9.9, shift = {0.0, 0.0}, color = {r = 0.75, g = 1.0, b = 0.0}}
-}
+colors["yellowgreen"].order = "b-c-b[yellowgreen]"
+colors["yellowgreen"].entity_glow = {intensity = 0.6, size = 9.9, shift = {0.0, 0.0}, color = {r = 0.75, g = 1.0, b = 0.0}}
 
 --green
-colors.green = {
-	subgroup = "reactor-mask",
-	order = "b-d-a[green]",
-	item_icon = {icon = "__base__/graphics/icons/nuclear-reactor.png", size = 32},
-	entity_icon = {icon = "__base__/graphics/icons/nuclear-reactor.png", size = 32},
-	entity_light_img = {img = "__base__/graphics/entity/nuclear-reactor/reactor-lights-color.png", width = 160, height = 160},
-	entity_light_hrimg = {img = "__base__/graphics/entity/nuclear-reactor/hr-reactor-lights-color.png", width = 320, height = 320, scale = 0.5},
-	entity_glow = {intensity = 0.6, size = 9.9, shift = {0.0, 0.0}, color = {r = 0.0, g = 1.0, b = 0.0}}
-}
+colors["green"].order = "b-d-a[green]"
+colors["green"].item_icon = {icon = "__base__/graphics/icons/nuclear-reactor.png", size = 32}
+colors["green"].entity_icon = {icon = "__base__/graphics/icons/nuclear-reactor.png", size = 32}
+colors["green"].entity_light_img = {img = "__base__/graphics/entity/nuclear-reactor/reactor-lights-color.png", width = 160, height = 160}
+colors["green"].entity_light_hrimg = {img = "__base__/graphics/entity/nuclear-reactor/hr-reactor-lights-color.png", width = 320, height = 320, scale = 0.5}
+colors["green"].entity_glow = {intensity = 0.6, size = 9.9, shift = {0.0, 0.0}, color = {r = 0.0, g = 1.0, b = 0.0}}
 
 --cyan
-colors.cyan = {
-	subgroup = "reactor-mask",
-	order = "b-e-a[cyan]",
-	item_icon = {icon = "__nuclear-fuel-cycle__/graphics/icons/nuclear-reactor-cyan.png", size = 32},
-	entity_icon = {icon = "__nuclear-fuel-cycle__/graphics/icons/nuclear-reactor-cyan.png", size = 32},
-	entity_light_img = {img = "__nuclear-fuel-cycle__/graphics/entity/reactor-lights-color-cyan.png", width = 160, height = 160},
-	entity_light_hrimg = {img = "__nuclear-fuel-cycle__/graphics/entity/hr-reactor-lights-color-cyan.png", width = 320, height = 320, scale = 0.5},
-	entity_glow = {intensity = 0.6, size = 9.9, shift = {0.0, 0.0}, color = {r = 0.0, g = 1.0, b = 0.94}}
-}
+colors["cyan"].order = "b-e-a[cyan]"
+colors["cyan"].entity_glow = {intensity = 0.6, size = 9.9, shift = {0.0, 0.0}, color = {r = 0.0, g = 1.0, b = 0.94}}
 
 --blue
-colors.blue = {
-	subgroup = "reactor-mask",
-	order = "b-f-a[blue]",
-	item_icon = {icon = "__nuclear-fuel-cycle__/graphics/icons/nuclear-reactor-blue.png", size = 32},
-	entity_icon = {icon = "__nuclear-fuel-cycle__/graphics/icons/nuclear-reactor-blue.png", size = 32},
-	entity_light_img = {img = "__nuclear-fuel-cycle__/graphics/entity/reactor-lights-color-blue.png", width = 160, height = 160},
-	entity_light_hrimg = {img = "__nuclear-fuel-cycle__/graphics/entity/hr-reactor-lights-color-blue.png", width = 320, height = 320, scale = 0.5},
-	entity_glow = {intensity = 0.8, size = 9.9, shift = {0.0, 0.0}, color = {r = 0.0, g = 0.65, b = 0.94}}
-}
+colors["blue"].order = "b-f-a[blue]"
+colors["blue"].entity_glow = {intensity = 0.8, size = 9.9, shift = {0.0, 0.0}, color = {r = 0.0, g = 0.65, b = 0.94}}
