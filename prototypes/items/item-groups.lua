cad79c5c2c749b45674168b2663ebdff19f0168d
muink <hukk1996@gmail.com>
2019-02-04 13:43:44 +0800

    主要物品与配方重新分类

--- prototypes/items/item-groups.lua
+++ prototypes/items/item-groups.lua
@@ -3,12 +3,24 @@ data:extend({
 		type = "item-subgroup",
 		name = "nuclear-processing",
 		group = "intermediate-products",
-		order = "b-a"
+		order = "c-a"
+	},
+	{
+		type = "item-subgroup",
+		name = "nuclear-reprocessing",
+		group = "intermediate-products",
+		order = "c-b"
 	},
 	{
 		type = "item-subgroup",
 		name = "nuclear-product",
 		group = "intermediate-products",
-		order = "b-b"
+		order = "c-c"
+	},
+	{
+		type = "item-subgroup",
+		name = "nuclear-fuel",
+		group = "intermediate-products",
+		order = "c-d"
 	}
 })
