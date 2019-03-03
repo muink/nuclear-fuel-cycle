e48055d21052c4ce080bd1c92be92b3a52769290
muink <hukk1996@gmail.com>
2019-03-03 20:27:56 +0800

    通知文本调整

--- migrations/nuclear-fuel-cycle_0.1.0.lua
+++ migrations/nuclear-fuel-cycle_0.1.0.lua
@@ -20,6 +20,5 @@ for index, force in pairs(game.forces) do
 		recipes["mox-fuel-cell-from-WGPu-239"].enabled = true
 		recipes["mox-fuel-reprocessing"].enabled = true
 	end
-	force.print({"message.tech-migration", "0.1.0"}, {r=1,g=0.75,b=0,a=1})
-	--force.print({message.mod-title, {"message.tech-migration", "0.1.0"}}, {r=1,g=0.75,b=0,a=1})
+	force.print({"message.head", {"message.tech-migration", "0.1.0"}}, {r=1,g=0.75,b=0,a=1})
 end
